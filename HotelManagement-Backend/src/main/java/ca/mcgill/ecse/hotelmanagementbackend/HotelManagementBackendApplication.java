581c5a97c6d1555991eee9ad80b4578fe4c1b964
Yuting Li <yuting.li@mail.mcgill.ca>
2023-09-25 17:55:35 -0400

    Reinit Project

---
+++ HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/HotelManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse.hotelmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementBackendApplication.class, args);
+    }
+
+}
---
+++ HotelManagement-Backend/src/test/java/ca/mcgill/ecse/hotelmanagementbackend/HotelManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse.hotelmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
