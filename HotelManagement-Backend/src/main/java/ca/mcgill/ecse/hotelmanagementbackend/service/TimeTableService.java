4d3a98997ab661260f27c47efe9d873c51d01375
Yuting Li <yuting.li@mail.mcgill.ca>
2023-09-28 11:40:49 -0400

    Update TimeTableService.

--- HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/service/TimeTableService.java
+++ HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/service/TimeTableService.java
@@ -4,6 +4,7 @@ import ca.mcgill.ecse.hotelmanagementbackend.entity.TimeTable;
 import ca.mcgill.ecse.hotelmanagementbackend.repository.TimeTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,18 +13,22 @@ public class TimeTableService {
     @Autowired
     TimeTableRepository timeTableRepository;
 
+    @Transactional(readOnly = true)
     public List<TimeTable> findAll() {
         return timeTableRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public TimeTable findById(Long id) {
         return timeTableRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public void save(TimeTable timeTable) {
         timeTableRepository.save(timeTable);
     }
 
+    @Transactional
     public void deleteById(Long id) {
         timeTableRepository.deleteById(id);
     }
