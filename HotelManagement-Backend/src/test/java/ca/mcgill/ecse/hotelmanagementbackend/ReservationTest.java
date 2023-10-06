ae26ce8193dfa54322df0e54b89832a0efe4311c
xiaoshanyichen <yichen.cao@mail.mcgill.ca>
2023-10-05 23:25:13 -0400

    Updated more tests

--- HotelManagement-Backend/src/test/java/ca/mcgill/ecse/hotelmanagementbackend/ReservationTest.java
+++ HotelManagement-Backend/src/test/java/ca/mcgill/ecse/hotelmanagementbackend/ReservationTest.java
@@ -3,9 +3,12 @@ package ca.mcgill.ecse.hotelmanagementbackend;
 import ca.mcgill.ecse.hotelmanagementbackend.entity.Customer;
 import ca.mcgill.ecse.hotelmanagementbackend.entity.Hotel;
 import ca.mcgill.ecse.hotelmanagementbackend.entity.Reservation;
+import ca.mcgill.ecse.hotelmanagementbackend.entity.Room;
+import ca.mcgill.ecse.hotelmanagementbackend.enumeration.RoomType;
 import ca.mcgill.ecse.hotelmanagementbackend.service.CustomerService;
 import ca.mcgill.ecse.hotelmanagementbackend.service.HotelService;
 import ca.mcgill.ecse.hotelmanagementbackend.service.ReservationService;
+import ca.mcgill.ecse.hotelmanagementbackend.service.RoomService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +29,8 @@ public class ReservationTest {
     private HotelService hotelService;
     @Autowired
     private CustomerService customerService;
+    @Autowired
+    private RoomService roomService;
 
     @Test
     @Transactional
@@ -38,7 +43,7 @@ public class ReservationTest {
         Reservation reservation = new Reservation(new Date());
         reservationService.save(reservation);
         reservations.add(reservation);
-        assertEquals("checks if reservation is properly saved", reservations, customer1.getReservations());
+        assertEquals("checks if reservation is properly saved", reservations, customer1.getReservationsForCustomer());
     }
 
     @Test
@@ -47,11 +52,37 @@ public class ReservationTest {
         Hotel hotel = new Hotel();
         hotelService.save(hotel);
         Customer customer1 = new Customer("Test1", "test1", "test1@test.com", "test1", hotel);
+        Customer customer2 = new Customer("Test2", "test2", "test2@test.com", "test2", hotel);
         customerService.save(customer1);
         List<Reservation> reservations = new ArrayList<>();
         Reservation reservation = new Reservation(new Date());
+        reservation.setCustomer(customer1);
+        reservationService.save(reservation);
         //test read
-        assertEquals("test read Reservation Attributes(id)", customer1.getId(), reservation.set);
+        assertEquals("test read Reservation Attributes(customer)", customer1, reservation.getCustomer());
+        //test write
+        reservation.setCustomer(customer2);
+        reservationService.save(reservation);
+        assertEquals("test write Reservation Attributes(customer)", customer2, reservation.getCustomer());
     }
 
+    @Test
+    @Transactional
+    void testReadAndWriteReference(){
+        Reservation reservation = new Reservation(new Date());
+        List<Room> rooms = new ArrayList<>();
+        Room room1 = new Room(RoomType.DELUXE, 1000);
+        roomService.save(room1);
+        Room room2 = new Room(RoomType.LUXURY, 1000);
+        roomService.save(room2);
+        Room room3 = new Room(RoomType.SUITE, 1000);
+        roomService.save(room3);
+        rooms.add(room1);
+        rooms.add(room2);
+        rooms.add(room3);
+        // test read and write reference between room reservation
+        reservation.setRooms(rooms);
+        reservationService.save(reservation);
+        assertEquals("checks if reservation can read and write its attribute which is an instance of Room class", rooms, reservation.getRooms());
+    }
 }
---
+++ HotelManagement-Backend/src/test/java/ca/mcgill/ecse/hotelmanagementbackend/TimeTableTest.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse.hotelmanagementbackend;
+
+import ca.mcgill.ecse.hotelmanagementbackend.entity.*;
+import ca.mcgill.ecse.hotelmanagementbackend.enumeration.RoomType;
+import ca.mcgill.ecse.hotelmanagementbackend.service.*;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+@SpringBootTest
+public class TimeTableTest {
+    @Autowired
+    private RoomService roomService;
+    @Autowired
+    private HotelService hotelService;
+
+    void
+}
