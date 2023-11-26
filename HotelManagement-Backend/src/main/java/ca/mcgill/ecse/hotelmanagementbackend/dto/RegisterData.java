0915a94811dc825d2fe923c947cb22f94985dcca
Yuting Li <yuting.li@mail.mcgill.ca>
2023-11-25 23:57:31 -0500

    Add AllArgsConstructor

--- HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/dto/RegisterData.java
+++ HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/dto/RegisterData.java
@@ -1,10 +1,12 @@
 package ca.mcgill.ecse.hotelmanagementbackend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class RegisterData {
     private String name;
     private String username;
