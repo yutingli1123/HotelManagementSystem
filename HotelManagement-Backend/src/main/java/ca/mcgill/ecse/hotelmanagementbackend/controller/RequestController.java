120153cbd05424e8f6684a97ba0bfe6be9203311
Simiao <126015338+Simiaoooo@users.noreply.github.com>
2023-11-26 05:12:08 -0500

    delete some not used import in RequestController

--- HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/controller/RequestController.java
+++ HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/controller/RequestController.java
@@ -1,21 +1,9 @@
 package ca.mcgill.ecse.hotelmanagementbackend.controller;
 
-
-import ca.mcgill.ecse.hotelmanagementbackend.entity.Owner;
 import ca.mcgill.ecse.hotelmanagementbackend.entity.Request;
-import ca.mcgill.ecse.hotelmanagementbackend.service.OwnerService;
 import ca.mcgill.ecse.hotelmanagementbackend.service.RequestService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import ca.mcgill.ecse.hotelmanagementbackend.repository.ReservationRepository;
-import ca.mcgill.ecse.hotelmanagementbackend.entity.Request;
-import ca.mcgill.ecse.hotelmanagementbackend.repository.RequestRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 import java.util.List;
