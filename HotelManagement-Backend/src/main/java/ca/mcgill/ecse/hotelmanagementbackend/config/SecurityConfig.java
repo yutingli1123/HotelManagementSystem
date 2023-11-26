499614bf1a761078faa62eb6c51371c4c01609e3
Yuting Li <yuting.li@mail.mcgill.ca>
2023-11-26 16:47:07 -0500

    Add authorization

---
+++ HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package ca.mcgill.ecse.hotelmanagementbackend.config;
+
+import ca.mcgill.ecse.hotelmanagementbackend.config.filter.ApiKeyAuthFilter;
+import ca.mcgill.ecse.hotelmanagementbackend.enumeration.Role;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() {
+        return authentication -> {
+            String principal = (String) authentication.getPrincipal();
+            String[] formattedToken = principal.split(" ");
+            if (formattedToken[0].equals("Bearer")) {
+                Role role = JWT.require(Algorithm.HMAC256(secretKey)).build().verify(formattedToken[1]).getClaim("role").as(Role.class);
+                String username = JWT.require(Algorithm.HMAC256(secretKey)).build().verify(formattedToken[1]).getIssuer();
+                if (role != null) {
+                    List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+                    authentication = new PreAuthenticatedAuthenticationToken(username, principal, authorities);
+                    authentication.setAuthenticated(true);
+                }
+            }
+            return authentication;
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        ApiKeyAuthFilter filter = new ApiKeyAuthFilter(HttpHeaders.AUTHORIZATION);
+        filter.setAuthenticationManager(customAuthenticationManager());
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilter(filter)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.POST, "/api/v1/login")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/register")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/reservations/**")
+                        .authenticated()
+                        .anyRequest()
+                        .hasAnyAuthority("ROLE_OWNER", "ROLE_EMPLOYEE")
+                );
+        return http.build();
+    }
+}
