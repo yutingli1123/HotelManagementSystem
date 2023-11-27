6ebaed5d2540c933e722ae21482f2398666a99c4
Yuting Li <yuting.li@mail.mcgill.ca>
2023-11-26 19:55:59 -0500

    Add refresh method

--- HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/controller/LoginController.java
+++ HotelManagement-Backend/src/main/java/ca/mcgill/ecse/hotelmanagementbackend/controller/LoginController.java
@@ -11,6 +11,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
@@ -82,5 +83,34 @@ public class LoginController {
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<HashMap<String,String>> register(@RequestBody String refreshToken) {
+        Date now = new Date();
+        Date expire = new Date(now.getTime() + 15 * 60 * 1000);
+        Date refresh_expire = new Date(now.getTime() + 20 * 60 * 1000);
+        String username = JWT.require(Algorithm.HMAC256(secretKey)).build().verify(refreshToken).getIssuer();
+
+        if (customerService.findByUsername(username) != null) {
+                HashMap<String, String> responseData = new HashMap<>();
+                responseData.put("token",JWT.create().withIssuer(username).withClaim("role", Role.CUSTOMER.toString()).withIssuedAt(now).withExpiresAt(expire).sign(Algorithm.HMAC256(secretKey)));
+                responseData.put("refresh_token", JWT.create().withIssuer(username).withIssuedAt(now).withExpiresAt(refresh_expire).sign(Algorithm.HMAC256(secretKey)));
+                return ResponseEntity.ok(responseData);
+        } else if (employeeService.findByUsername(username) != null) {
+
+                HashMap<String, String> responseData = new HashMap<>();
+                responseData.put("token",JWT.create().withIssuer(username).withClaim("role", Role.EMPLOYEE.toString()).withIssuedAt(now).withExpiresAt(expire).sign(Algorithm.HMAC256(secretKey)));
+                responseData.put("refresh_token", JWT.create().withIssuer(username).withIssuedAt(now).withExpiresAt(refresh_expire).sign(Algorithm.HMAC256(secretKey)));
+                return ResponseEntity.ok(responseData);
+        } else if (ownerService.findByUsername(username) != null) {
+
+                HashMap<String, String> responseData = new HashMap<>();
+                responseData.put("token",JWT.create().withIssuer(username).withClaim("role", Role.OWNER.toString()).withIssuedAt(now).withExpiresAt(expire).sign(Algorithm.HMAC256(secretKey)));
+                responseData.put("refresh_token", JWT.create().withIssuer(username).withIssuedAt(now).withExpiresAt(refresh_expire).sign(Algorithm.HMAC256(secretKey)));
+                return ResponseEntity.ok(responseData);
+
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
